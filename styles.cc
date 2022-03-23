2eec827f4b500665e16a6b1a13ad43c24eda3280
storm-codes <cjxninja@gmail.com>
2022-03-23 14:52:01 +0200

    Add Styles

---
+++ styles.cc
@@ -0,0 +1,111 @@
+
+
+#head {
+    background-color: white;
+    width: 100%;
+    margin: 0rem,0rem,15rem,0rem;
+    filter: drop-shadow(0rem 1rem 0.75rem rgba(109, 109, 109, 0.418));
+    
+    }
+    
+    #title {
+        font-family: 'Righteous', 'Courier New', Courier, monospace;
+        font-size: 3rem;
+        margin-top: 1rem;
+        margin-bottom: 0.5rem;
+    }
+    
+    #title {
+        color:rgb(0, 0, 0);
+    }
+    
+    .subheading {
+        color:rgb(59, 59, 59);
+    }
+    
+    #main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: rgb(255, 255, 255);
+        padding: 3rem,6rem,20rem,6rem;
+    }
+    
+    #tribute-info {
+        font-family: 'Inconsolata', monospace;
+        font-weight: 400;
+        font-size: large;
+    }
+    
+    body {
+        margin: 0;
+        font-family: 'Inconsolata', 'Courier New', Courier, monospace;
+        text-align: center;
+    }
+    
+    .image {
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+    }
+    
+    #img-caption {
+        background-color: rgb(255, 255, 255);
+        padding: 1rem;
+        border-style: dashed;
+        border-color: rgb(47, 255, 210);
+    }
+    
+    #img-div {
+        background-image: url(https://cdnb.artstation.com/p/assets/images/images/035/877/167/large/gregory-ligman-brickwall.jpg?1616129547);
+        padding: 2rem;
+        align-items: center;
+    }
+    
+    #info-boy {
+        background-color: rgb(255, 255, 255);
+        border-style: double;
+        border-color: rgb(90, 90, 90);
+        width: 100%;
+        max-width: 1000px;
+        margin: 3rem, auto;
+        padding: auto;
+    }
+    
+    a:hover {
+        color: black;
+        background-color: rgb(241, 239, 85);
+    }
+    
+    #cta {
+        align-items: center;
+        width: 100%;
+        max-width: 400px;
+        margin: 5rem 1rem 5rem 1rem;
+        padding: 2rem 4rem;
+        background-color: rgb(255, 245, 224);
+        display: block;
+        
+    }
+    
+    .cta-button {
+        color: rgb(67, 71, 75);
+        background-color: rgb(170, 170, 170);
+        border: rgb(67, 71, 75);
+        border-style: solid;
+        border-width: 2px;
+        padding: 1rem;
+        margin: 1rem;
+    }
+    
+    
+    .cta-button:hover {
+        color: rgb(58, 255, 19);
+        background-color: rgb(0, 0, 0);
+        border: rgb(58, 255, 19);
+        border-style: solid;
+        border-width: 2px;
+        padding: 1rem;
+        margin: 1rem;
+        display: block;
+    }
